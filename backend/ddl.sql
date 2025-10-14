6e54a6db707578021c1157f8a1d06d40b5ddbf34
Jorge <erudito.tv@gmail.com>
2025-10-14 15:10:47 -0500

    v1

--- backend/ddl.sql
+++ backend/ddl.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE naruto
-WITH
-    OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' TABLESPACE = pg_default CONNECTION
-LIMIT
-    = -1;
-
 CREATE TABLE
     IF NOT EXISTS public.clan (
         clan_id BIGSERIAL PRIMARY KEY,
