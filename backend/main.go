0963c53ba0dec8f427ab6733619711f94fa70e25
Jorge <erudito.tv@gmail.com>
2025-10-14 16:01:34 -0500

    v1

--- backend/main.go
+++ backend/main.go
@@ -52,8 +52,8 @@ func main() {
 	app := fiber.New()
 
 	allowedOrigins := map[string]bool{
-		"http://localhost:5173":                 true, // desarrollo
-		"http://docker.between-bytes.tech:3000": true, // producción
+		"http://localhost:5173":            true, // desarrollo
+		"http://docker.between-bytes.tech": true, // producción
 	}
 
 	app.Use(cors.New(cors.Config{
